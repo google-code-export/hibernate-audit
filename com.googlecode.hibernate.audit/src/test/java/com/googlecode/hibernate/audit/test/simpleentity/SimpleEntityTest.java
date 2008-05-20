8dfd5363739a8ef43d60d567748177ed0cd95be7
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-05-20 17:56:49 +0000

    ingnoring 'target' which is a local artifact of 'mvn site'. See https://jira.novaordis.org/browse/HBA-2

--- com.googlecode.hibernate.audit/src/test/java/com/googlecode/hibernate/audit/test/simpleentity/SimpleEntityTest.java
+++ com.googlecode.hibernate.audit/src/test/java/com/googlecode/hibernate/audit/test/simpleentity/SimpleEntityTest.java
@@ -14,7 +14,8 @@ public class SimpleEntityTest extends AuditTest {
 	private static final Logger LOG = Logger.getLogger(SimpleEntityTest.class);
 
 	@Test(enabled = true)
-	public void test() {
+
+    public void test() {
 		Session session = getSession();
 
 		SimpleEntity entity = new SimpleEntity();
