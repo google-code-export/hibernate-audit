58ff31a323b0716de63cd2b0402d8201edd6d271
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-06-23 05:54:52 +0000

    new listener abstract base

---
+++ com.googlecode.hibernate.audit/src/main/java/com/googlecode/hibernate/audit/listener/AbstractAuditEventListener.java
@@ -0,0 +1,34 @@
+package com.googlecode.hibernate.audit.listener;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author <a href="mailto:chobantonov@gmail.com">Petko Chobantonov</a>
+ * @author <a href="mailto:jchobantonov@gmail.com">Zhelyazko Chobantonov</a>
+ * @author <a href="mailto:kchobantonov@gmail.com">Krasimir Chobantonov</a>
+ * @author <a href="mailto:ovidiu@feodorov.com">Ovidiu Feodorov</a>
+ */
+abstract class AbstractAuditEventListener implements AuditEventListener
+{
+    // Constants -----------------------------------------------------------------------------------
+
+    private static final Logger log = Logger.getLogger(AbstractAuditEventListener.class);
+
+    // Static --------------------------------------------------------------------------------------
+
+    // Attributes ----------------------------------------------------------------------------------
+
+    // Constructors --------------------------------------------------------------------------------
+
+    // AuditEventListener implementation -----------------------------------------------------------
+
+    // Public --------------------------------------------------------------------------------------
+
+    // Package protected ---------------------------------------------------------------------------
+
+    // Protected -----------------------------------------------------------------------------------
+
+    // Private -------------------------------------------------------------------------------------
+
+    // Inner classes -------------------------------------------------------------------------------
+}
