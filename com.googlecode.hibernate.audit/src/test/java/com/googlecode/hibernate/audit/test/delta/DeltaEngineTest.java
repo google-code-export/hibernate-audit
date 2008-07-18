8821513907a9b7ebec5c0fd92070e623e4b34124
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-07-18 05:20:31 +0000

    enabling all delta tests

--- com.googlecode.hibernate.audit/src/test/java/com/googlecode/hibernate/audit/test/delta/DeltaEngineTest.java
+++ com.googlecode.hibernate.audit/src/test/java/com/googlecode/hibernate/audit/test/delta/DeltaEngineTest.java
@@ -41,7 +41,7 @@ public class DeltaEngineTest extends JTATransactionTest
 
     // Public --------------------------------------------------------------------------------------
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testNoSuchEntity() throws Exception
     {
         AnnotationConfiguration config = new AnnotationConfiguration();
@@ -73,7 +73,7 @@ public class DeltaEngineTest extends JTATransactionTest
         }
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testInvalidId() throws Exception
     {
         AnnotationConfiguration config = new AnnotationConfiguration();
@@ -106,7 +106,7 @@ public class DeltaEngineTest extends JTATransactionTest
         }
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testNoSuchAuditTransaction() throws Exception
     {
         AnnotationConfiguration config = new AnnotationConfiguration();
@@ -140,7 +140,7 @@ public class DeltaEngineTest extends JTATransactionTest
         }
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testNoSuchTypeAudited() throws Exception
     {
         AnnotationConfiguration config = new AnnotationConfiguration();
@@ -196,7 +196,7 @@ public class DeltaEngineTest extends JTATransactionTest
         }
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void testNoSuchEntityIdInDatabase() throws Exception
     {
         AnnotationConfiguration config = new AnnotationConfiguration();
