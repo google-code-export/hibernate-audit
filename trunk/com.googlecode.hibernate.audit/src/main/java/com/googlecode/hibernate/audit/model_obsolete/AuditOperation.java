ac41a3934367907f21fe404d94c54da4eed0b745
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-07-27 04:55:54 +0000

    Getting rid of obsolete classes https://jira.novaordis.org/browse/HBA-62

--- trunk/com.googlecode.hibernate.audit/src/main/java/com/googlecode/hibernate/audit/model_obsolete/AuditOperation.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.hibernate.audit.model_obsolete;
-
-public enum AuditOperation {
-	INSERT,
-	UPDATE,
-	DELETE
-}
--- trunk/com.googlecode.hibernate.audit/src/main/java/com/googlecode/hibernate/audit/model_obsolete/transaction/record/field/value/AuditValue.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.hibernate.audit.model_obsolete.transaction.record.field.value;
-
-import javax.persistence.Embeddable;
-
-@Embeddable
-public abstract class AuditValue {
-	public abstract boolean isOfType(Class<? extends AuditValue> valueClass);
-}
