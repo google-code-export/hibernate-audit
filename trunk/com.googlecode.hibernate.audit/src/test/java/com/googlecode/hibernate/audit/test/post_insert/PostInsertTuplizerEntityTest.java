cd26af669d4ea1ff6bdfbab80ff76d49bd09bf37
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-08-19 18:51:21 +0000

    test case (it currently fails) for https://jira.novaordis.org/browse/HBA-90

--- trunk/com.googlecode.hibernate.audit/src/test/java/com/googlecode/hibernate/audit/test/post_insert/PostInsertTuplizerEntityTest.java
+++ trunk/com.googlecode.hibernate.audit/src/test/java/com/googlecode/hibernate/audit/test/post_insert/PostInsertTuplizerEntityTest.java
@@ -12,11 +12,14 @@ import com.googlecode.hibernate.audit.test.post_insert.data.XBTuplizer;
 import com.googlecode.hibernate.audit.test.post_insert.data.XC;
 import com.googlecode.hibernate.audit.test.post_insert.data.XCTuplizer;
 import com.googlecode.hibernate.audit.test.post_insert.data.XA2;
+import com.googlecode.hibernate.audit.test.post_insert.data.XA3;
+import com.googlecode.hibernate.audit.test.post_insert.data.XA3Tuplizer;
 import com.googlecode.hibernate.audit.HibernateAudit;
 import com.googlecode.hibernate.audit.model.AuditTransaction;
 
 import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
 import java.io.ByteArrayInputStream;
 
 /**
@@ -43,7 +46,7 @@ public class PostInsertTuplizerEntityTest extends JTATransactionTest
 
     // Public --------------------------------------------------------------------------------------
 
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void testManyToOne_OneIsTuplizer() throws Exception
     {
         Configuration config = new Configuration();
@@ -139,7 +142,7 @@ public class PostInsertTuplizerEntityTest extends JTATransactionTest
     /**
      * This currently fails https://jira.novaordis.org/browse/HBA-81
      */
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void testMissingMutatorThatMayBeSalvagedByTuplizer() throws Exception
     {
         Configuration config = new Configuration();
@@ -231,7 +234,7 @@ public class PostInsertTuplizerEntityTest extends JTATransactionTest
         }
     }
 
-    @Test(enabled = true)
+    @Test(enabled = false)
     public void testManyToOne_OneIsTuplizer_Collection() throws Exception
     {
         Configuration config = new Configuration();
@@ -345,6 +348,122 @@ public class PostInsertTuplizerEntityTest extends JTATransactionTest
     }
 
     @Test(enabled = true)
+    public void testManyToOne_BothAreTuplizers_Collection() throws Exception
+    {
+        Configuration config = new Configuration();
+        config.configure(getHibernateConfigurationFileName());
+
+        // we add metadata as XML otherwise we can't simulate the condition (JPA not expressive
+        // enough)
+
+        String xa3Mapping =
+            "<?xml version='1.0'?>\n" +
+            "<!DOCTYPE hibernate-mapping PUBLIC\n" +
+            "    \"-//Hibernate/Hibernate Mapping DTD 3.0//EN\"\n" +
+            "    \"http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd\">\n" +
+            "<hibernate-mapping>\n" +
+            "   <class entity-name='XA3' name='com.googlecode.hibernate.audit.test.post_insert.data.XA3' table='XA3'>\n" +
+            "        <tuplizer entity-mode='pojo' class='com.googlecode.hibernate.audit.test.post_insert.data.XA3Tuplizer'/>\n" +
+            "        <id name='id' type='long'>\n" +
+            "            <generator class='native'/>\n" +
+            "        </id>\n" +
+            "        <set name='xbs' cascade='all'>\n" +
+            "            <key column='xa_id'/>\n" +
+            "            <one-to-many entity-name='XB'/>\n" +
+            "        </set>\n" +
+            "    </class>\n" +
+            "</hibernate-mapping>";
+
+        String xbMapping =
+            "<?xml version='1.0'?>\n" +
+            "<!DOCTYPE hibernate-mapping PUBLIC\n" +
+            "    \"-//Hibernate/Hibernate Mapping DTD 3.0//EN\"\n" +
+            "    \"http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd\">\n" +
+            "<hibernate-mapping>\n" +
+            "   <class entity-name='XB' name='com.googlecode.hibernate.audit.test.post_insert.data.XB' table='XB'>\n" +
+            "      <tuplizer entity-mode='pojo' class='com.googlecode.hibernate.audit.test.post_insert.data.XBTuplizer'/>\n" +
+            "      <id name='id' type='long'>\n" +
+            "         <generator class='native'/>\n" +
+            "      </id>\n" +
+            "      <property name='name' type='string'/>\n" +
+            "   </class>\n" +
+            "</hibernate-mapping>";
+
+        config.addInputStream(new ByteArrayInputStream(xa3Mapping.getBytes()));
+        config.addInputStream(new ByteArrayInputStream(xbMapping.getBytes()));
+
+        SessionFactory sf = null;
+
+        try
+        {
+            sf = config.buildSessionFactory();
+
+            HibernateAudit.enable(sf);
+
+            Session s = sf.openSession();
+            s.beginTransaction();
+
+            XA3 xa3 = new XA3();
+            XB xbone = new XB();
+            XB xbtwo = new XB();
+
+            XBTuplizer xbTuplizer = new XBTuplizer();
+            xbTuplizer.setPropertyValue(xbone, "name", "xbone");
+            xbTuplizer.setPropertyValue(xbtwo, "name", "xbtwo");
+
+            Set<XB> xbs = new HashSet<XB>();
+            xbs.add(xbone);
+            xbs.add(xbtwo);
+            XA3Tuplizer xa3Tuplizer = new XA3Tuplizer();
+            xa3Tuplizer.setPropertyValue(xa3, "xbs", xbs);
+
+            s.save("XA3", xa3);
+
+            s.getTransaction().commit();
+            s.close();
+
+            List<AuditTransaction> transactions = HibernateAudit.getTransactions(xa3.getId());
+            assert transactions.size() == 1;
+
+            XA3 base = new XA3();
+            HibernateAudit.delta(base, xa3.getId(), transactions.get(0).getId());
+
+//            Set<XB> restored = base.getXbs();
+//            assert restored.size() == 2;
+//
+//            for(XB xb: restored)
+//            {
+//                if (xbone.getId().equals(xb.getId()))
+//                {
+//                    assert "xbone".equals(xb.getName());
+//                }
+//                else if (xbtwo.getId().equals(xb.getId()))
+//                {
+//                    assert "xbtwo".equals(xb.getName());
+//                }
+//                else
+//                {
+//                    throw new Error("unexpected " + xb);
+//                }
+//            }
+        }
+        catch(Exception e)
+        {
+            log.error("test failed unexpectedly", e);
+            throw e;
+        }
+        finally
+        {
+            HibernateAudit.disableAll();
+
+            if (sf != null)
+            {
+                sf.close();
+            }
+        }
+    }
+
+    @Test(enabled = false)
     public void testManyToOne_OneIsTuplizer_EmptyCollection() throws Exception
     {
         Configuration config = new Configuration();
