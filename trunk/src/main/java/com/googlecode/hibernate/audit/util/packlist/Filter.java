05addae210c64d1fc3d662fce78081a1eb9a0fab
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-10-10 04:30:27 +0000

    temporary commit 'packlist'

---
+++ trunk/src/main/java/com/googlecode/hibernate/audit/util/packlist/Filter.java
@@ -0,0 +1,14 @@
+package com.googlecode.hibernate.audit.util.packlist;
+
+/**
+ * @author <a href="mailto:ovidiu@feodorov.com">Ovidiu Feodorov</a>
+ *
+ * Copyright 2008 Ovidiu Feodorov
+ *
+ * @version <tt>$Revision$</tt>
+ *
+ * $Id$
+ */
+public interface Filter
+{
+}
