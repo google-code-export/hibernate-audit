937354d657a9b26039463030e2c142eff79f6b38
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-10-21 05:27:33 +0000

    extra test case for WriteOnceCache-related ConcurrentModificationException (https://jira.novaordis.org/browse/HBA-138)

--- trunk/src/test/java/com/googlecode/hibernate/audit/test/util/wocache/WriteOnceCacheAndTransactionsTest.java
+++ trunk/src/test/java/com/googlecode/hibernate/audit/test/util/wocache/WriteOnceCacheAndTransactionsTest.java
@@ -7,9 +7,17 @@ import org.hibernate.engine.SessionFactoryImplementor;
 import org.hibernate.Session;
 import com.googlecode.hibernate.audit.test.base.JTATransactionTest;
 import com.googlecode.hibernate.audit.test.util.wocache.data.A;
+import com.googlecode.hibernate.audit.test.mock.jta.JTAUtil;
 import com.googlecode.hibernate.audit.HibernateAudit;
+import com.googlecode.hibernate.audit.util.wocache.WriteOnceCache;
+import com.googlecode.hibernate.audit.util.wocache.CacheQuery;
 import com.googlecode.hibernate.audit.model.AuditTransaction;
 
+import javax.naming.InitialContext;
+import javax.transaction.UserTransaction;
+import javax.transaction.TransactionManager;
+import javax.transaction.Transaction;
+import javax.transaction.Synchronization;
 import java.util.List;
 
 /**
@@ -38,66 +46,123 @@ public class WriteOnceCacheAndTransactionsTest extends JTATransactionTest
 
     // Public --------------------------------------------------------------------------------------
 
-
-//    /**
-//     * See https://jira.novaordis.org/browse/HBA-138
-//     */
-//    TODO Uncomment after fixing https://jira.novaordis.org/browse/HBA-138
-//    @Test(enabled = true)
-//    public void testSynchronizationConcurrentModificationException_UserTransaction()
-//        throws Exception
-//    {
-//        log.debug("testSynchronizationConcurrentModificationException");
-//
-//        AnnotationConfiguration config = new AnnotationConfiguration();
-//        config.configure(getHibernateConfigurationFileName());
-//        config.addAnnotatedClass(A.class);
-//        InitialContext ic = new InitialContext();
-//        SessionFactoryImplementor sf = null;
-//        UserTransaction ut = null;
-//
-//        try
-//        {
-//            sf = (SessionFactoryImplementor)config.buildSessionFactory();
-//
-//            HibernateAudit.startRuntime(sf.getSettings());
-//            HibernateAudit.register(sf);
-//
-//            ut = (UserTransaction)ic.lookup(getUserTransactionJNDIName());
-//
-//            ut.begin();
-//
-//            Session s = sf.getCurrentSession();
-//
-//            A a = new A();
-//            a.setS("alice");
-//            s.save(a);
-//
-//            ut.commit();
-//
-//            List<AuditTransaction> txs = HibernateAudit.getTransactions();
-//            assert txs.size() == 1;
-//        }
-//        finally
-//        {
-//            HibernateAudit.stopRuntime();
-//
-//            if (sf != null)
-//            {
-//                sf.close();
-//            }
-//
-//            if (ic != null)
-//            {
-//                ic.close();
-//            }
-//        }
-//    }
-
     /**
      * See https://jira.novaordis.org/browse/HBA-138
      */
     @Test(enabled = true)
+    public void testSynchronizationConcurrentModificationException_FireWOCacheFromSynchronization()
+        throws Exception
+    {
+        AnnotationConfiguration config = new AnnotationConfiguration();
+        config.configure(getHibernateConfigurationFileName());
+        config.addAnnotatedClass(A.class);
+        SessionFactoryImplementor sf = null;
+        InitialContext ic = null;
+        TransactionManager tm = null;
+
+        try
+        {
+             sf = (SessionFactoryImplementor)config.buildSessionFactory();
+
+            final WriteOnceCache<A> woCache = new WriteOnceCache<A>(sf);
+
+            ic = new InitialContext();
+            tm = (TransactionManager)ic.lookup(getTransactionManagerJNDIName());
+
+            tm.begin();
+            Transaction tx = tm.getTransaction();
+
+            tx.registerSynchronization(new Synchronization()
+            {
+                public void beforeCompletion()
+                {
+                    log.debug(">>> beforeCompletion()");
+
+                    try
+                    {
+                        woCache.get(new CacheQuery<A>(A.class, "s", "alice"));
+                    }
+                    catch(Exception e)
+                    {
+                        throw new IllegalStateException("failed to get from woCache", e);
+                    }
+                }
+
+                public void afterCompletion(int i)
+                {
+                    log.debug(">>> afterCompletion(" + JTAUtil.statusToString(i) + ")");
+                }
+            });
+
+            tx.commit();
+        }
+        finally
+        {
+            if (ic != null)
+            {
+                ic.close();
+            }
+        }
+    }
+
+    /**
+     * See https://jira.novaordis.org/browse/HBA-138
+     */
+    @Test(enabled = false)
+    public void testSynchronizationConcurrentModificationException_UserTransaction()
+        throws Exception
+    {
+        log.debug("testSynchronizationConcurrentModificationException");
+
+        AnnotationConfiguration config = new AnnotationConfiguration();
+        config.configure(getHibernateConfigurationFileName());
+        config.addAnnotatedClass(A.class);
+        InitialContext ic = new InitialContext();
+        SessionFactoryImplementor sf = null;
+        UserTransaction ut = null;
+
+        try
+        {
+            sf = (SessionFactoryImplementor)config.buildSessionFactory();
+
+            HibernateAudit.startRuntime(sf.getSettings());
+            HibernateAudit.register(sf);
+
+            ut = (UserTransaction)ic.lookup(getUserTransactionJNDIName());
+
+            ut.begin();
+
+            Session s = sf.getCurrentSession();
+
+            A a = new A();
+            a.setS("alice");
+            s.save(a);
+
+            ut.commit();
+
+            List<AuditTransaction> txs = HibernateAudit.getTransactions();
+            assert txs.size() == 1;
+        }
+        finally
+        {
+            HibernateAudit.stopRuntime();
+
+            if (sf != null)
+            {
+                sf.close();
+            }
+
+            if (ic != null)
+            {
+                ic.close();
+            }
+        }
+    }
+
+    /**
+     * See https://jira.novaordis.org/browse/HBA-138
+     */
+    @Test(enabled = false)
     public void testSynchronizationConcurrentModificationException_HibernateTransaction()
         throws Exception
     {
