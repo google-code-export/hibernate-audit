149f14098aaa32c90ee947f0950715c07ca193d8
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-10-30 20:18:53 +0000

    creating a branch for audit logging code that has all exceptions supressed

---
+++ branches/Branch_Supressed_Exceptions/src/test/java/com/googlecode/hibernate/audit/test/performance/data/s2/Scenario.java
@@ -0,0 +1,13 @@
+package com.googlecode.hibernate.audit.test.performance.data.s2;
+
+public interface Scenario
+{
+    int getPPerDCount();
+    int getEPerPCount();
+    int getEXPerPCount();
+    int getLPerPCount();
+    int getAPerPCount();
+    int getCLCPerPCount();
+    int getPPPerPCount();
+    int getPAPerPCount();
+}
