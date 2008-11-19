e568736ab07d1c5608f4f5b5e095cf210bdce9a1
ovidiu@feodorov.com <ovidiu@feodorov.com@5bd74da1-d548-0410-99f0-b708941b1824>
2008-11-19 21:19:12 +0000

    extra tests related to 'multiple logical groups' functionality https://jira.novaordis.org/browse/HBA-178

---
+++ trunk/src/test/java/com/googlecode/hibernate/audit/test/logical_group_id/MultipleLogicalGroupsPerTransactionTest.java
@@ -0,0 +1,88 @@
+package com.googlecode.hibernate.audit.test.logical_group_id;
+
+import org.testng.annotations.Test;
+import org.apache.log4j.Logger;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.engine.SessionFactoryImplementor;
+import org.hibernate.Session;
+import com.googlecode.hibernate.audit.test.base.JTATransactionTest;
+import com.googlecode.hibernate.audit.test.HibernateAuditTest;
+import com.googlecode.hibernate.audit.test.logical_group_id.data.A;
+import com.googlecode.hibernate.audit.test.logical_group_id.data.B;
+import com.googlecode.hibernate.audit.HibernateAudit;
+import com.googlecode.hibernate.audit.LogicalGroupIdProvider;
+
+/**
+ * @author <a href="mailto:ovidiu@feodorov.com">Ovidiu Feodorov</a>
+ *
+ * Copyright 2008 Ovidiu Feodorov
+ *
+ * @version <tt>$Revision$</tt>
+ *
+ * $Id$
+ */
+@Test(sequential = true)
+public class MultipleLogicalGroupsPerTransactionTest extends JTATransactionTest
+{
+    // Constants -----------------------------------------------------------------------------------
+
+    private static final Logger log = Logger.getLogger(HibernateAuditTest.class);
+
+    // Static --------------------------------------------------------------------------------------
+
+    // Attributes ----------------------------------------------------------------------------------
+
+    // Constructors --------------------------------------------------------------------------------
+
+    // Public --------------------------------------------------------------------------------------
+
+    @Test(enabled = true)
+    public void test() throws Exception
+    {
+        log.debug("test");
+
+        AnnotationConfiguration config = new AnnotationConfiguration();
+        config.configure(getHibernateConfigurationFileName());
+        config.addAnnotatedClass(A.class);
+        config.addAnnotatedClass(B.class);
+        SessionFactoryImplementor sf = null;
+
+        try
+        {
+            sf = (SessionFactoryImplementor)config.buildSessionFactory();
+
+            HibernateAudit.startRuntime(sf.getSettings());
+            LogicalGroupIdProvider lgip = null;
+            HibernateAudit.register(sf, lgip);
+
+            Session s = sf.openSession();
+            s.beginTransaction();
+            A a = new A();
+            a.setName("something");
+            s.save(a);
+
+            B b = new B();
+            b.setName("something else");
+            s.save(b);
+
+            s.getTransaction().commit();
+        }
+        finally
+        {
+            HibernateAudit.stopRuntime();
+
+            if (sf != null)
+            {
+                sf.close();
+            }
+        }
+    }
+
+    // Package protected ---------------------------------------------------------------------------
+
+    // Protected -----------------------------------------------------------------------------------
+
+    // Private -------------------------------------------------------------------------------------
+
+    // Inner classes -------------------------------------------------------------------------------
+}
