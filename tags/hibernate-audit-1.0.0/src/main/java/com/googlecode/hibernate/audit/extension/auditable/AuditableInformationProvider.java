bdcd35d6a1456826fac478dd33e38bb95a2e25d5
kchobantonov <kchobantonov@5bd74da1-d548-0410-99f0-b708941b1824>
2009-02-13 04:54:24 +0000

    remove the tag because of maven plugin failures

--- tags/hibernate-audit-1.0.0/src/main/java/com/googlecode/hibernate/audit/extension/auditable/AuditableInformationProvider.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.hibernate.audit.extension.auditable;
-
-public interface AuditableInformationProvider {
-
-	boolean isAuditable(String entityName);
-
-	boolean isAuditable(String entityName, String propertyName);
-}
