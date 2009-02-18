24b3c9a4898ae97f0f7a75de3d78d32f6c9d15ed
kchobantonov <kchobantonov@5bd74da1-d548-0410-99f0-b708941b1824>
2009-02-18 17:19:46 +0000

    [maven-release-plugin]  copy for tag hibernate-audit-1.0.3

---
+++ tags/hibernate-audit-1.0.3/src/main/java/com/googlecode/hibernate/audit/configuration/AuditConfiguration.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2009 Krasimir Chobantonov <kchobantonov@yahoo.com>
+ * This file is part of Hibernate Audit.
+
+ * Hibernate Audit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Hibernate Audit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with Hibernate Audit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.googlecode.hibernate.audit.configuration;
+
+import org.hibernate.cfg.Configuration;
+
+import com.googlecode.hibernate.audit.extension.ExtensionManager;
+import com.googlecode.hibernate.audit.synchronization.AuditSynchronizationManager;
+
+public class AuditConfiguration {
+    private AuditSynchronizationManager auditSynchronizationManager = new AuditSynchronizationManager(this);
+    private Configuration configuration;
+    private ExtensionManager extensionManager = new ExtensionManager();
+
+    public AuditConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public Configuration getAuditedConfiguration() {
+        return configuration;
+    }
+
+    public AuditSynchronizationManager getAuditSynchronizationManager() {
+        return auditSynchronizationManager;
+    }
+
+    public ExtensionManager getExtensionManager() {
+        return extensionManager;
+    }
+
+}
---
+++ tags/hibernate-audit-1.0.3/src/main/java/com/googlecode/hibernate/audit/extension/event/AuditLogicalGroupProvider.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2009 Krasimir Chobantonov <kchobantonov@yahoo.com>
+ * This file is part of Hibernate Audit.
+
+ * Hibernate Audit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Hibernate Audit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with Hibernate Audit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.googlecode.hibernate.audit.extension.event;
+
+import org.hibernate.Session;
+
+import com.googlecode.hibernate.audit.model.AuditEvent;
+import com.googlecode.hibernate.audit.model.AuditLogicalGroup;
+
+public interface AuditLogicalGroupProvider {
+
+    AuditLogicalGroup getAuditLogicalGroup(Session session, AuditEvent auditEvent);
+}
