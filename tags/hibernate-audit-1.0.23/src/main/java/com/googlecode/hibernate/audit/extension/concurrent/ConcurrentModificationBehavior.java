d672ec48dfee673c19dba4bbb15fe4fe4ce432b2
kchobantonov <kchobantonov@5bd74da1-d548-0410-99f0-b708941b1824>
2010-07-28 22:39:56 +0000

    undo

--- tags/hibernate-audit-1.0.23/src/main/java/com/googlecode/hibernate/audit/extension/concurrent/ConcurrentModificationBehavior.java
+++
@@ -1,5 +0,0 @@
-package com.googlecode.hibernate.audit.extension.concurrent;
-
-public enum ConcurrentModificationBehavior {
-    THROW_EXCEPTION, LOG
-}
--- tags/hibernate-audit-1.0.23/src/main/java/com/googlecode/hibernate/audit/extension/concurrent/ConcurrentModificationLevelCheck.java
+++
@@ -1,5 +0,0 @@
-package com.googlecode.hibernate.audit.extension.concurrent;
-
-public enum ConcurrentModificationLevelCheck {
-    OBJECT, PROPERTY
-}
