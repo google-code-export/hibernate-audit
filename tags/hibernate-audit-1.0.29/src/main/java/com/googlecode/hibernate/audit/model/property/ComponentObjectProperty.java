7fb8f00de48c9b52073a7d6a88689d839424188d
kchobantonov@gmail.com <kchobantonov@gmail.com@5bd74da1-d548-0410-99f0-b708941b1824>
2011-08-04 16:22:35 +0000

    [maven-release-plugin]  copy for tag hibernate-audit-1.0.29

---
+++ tags/hibernate-audit-1.0.29/src/main/java/com/googlecode/hibernate/audit/model/property/ComponentObjectProperty.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2009 Krasimir Chobantonov <kchobantonov@yahoo.com>
+ * This file is part of Hibernate Audit.
+
+ * Hibernate Audit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Hibernate Audit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with Hibernate Audit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.googlecode.hibernate.audit.model.property;
+
+import com.googlecode.hibernate.audit.model.object.ComponentAuditObject;
+
+public class ComponentObjectProperty extends AuditObjectProperty {
+    protected ComponentAuditObject targetComponentAuditObject;
+
+    public ComponentAuditObject getTargetComponentAuditObject() {
+        return targetComponentAuditObject;
+    }
+
+    public void setTargetComponentAuditObject(ComponentAuditObject targetComponentAuditObject) {
+        this.targetComponentAuditObject = targetComponentAuditObject;
+    }
+}
