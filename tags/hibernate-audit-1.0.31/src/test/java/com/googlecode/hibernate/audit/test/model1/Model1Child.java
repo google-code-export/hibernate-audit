14154b06c09d1f6a0eb3041729ec95676312b2d4
kchobantonov@gmail.com <kchobantonov@gmail.com@5bd74da1-d548-0410-99f0-b708941b1824>
2011-09-22 03:23:57 +0000

    [maven-release-plugin]  copy for tag hibernate-audit-1.0.31

---
+++ tags/hibernate-audit-1.0.31/src/test/java/com/googlecode/hibernate/audit/test/model1/Model1Child.java
@@ -0,0 +1,56 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.googlecode.hibernate.audit.test.model1;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Child</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link com.googlecode.hibernate.audit.test.model1.Model1Child#getParent <em>Parent</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see com.googlecode.hibernate.audit.test.model1.Model1Package#getModel1Child()
+ * @model
+ * @generated
+ */
+public interface Model1Child extends Model1Person {
+    /**
+     * Returns the value of the '<em><b>Parent</b></em>' container reference.
+     * It is bidirectional and its opposite is '{@link com.googlecode.hibernate.audit.test.model1.Model1Parent#getChildren <em>Children</em>}'.
+     * <!-- begin-user-doc -->
+     * <p>
+     * If the meaning of the '<em>Parent</em>' container reference isn't clear,
+     * there really should be more of a description here...
+     * </p>
+     * <!-- end-user-doc -->
+     * @return the value of the '<em>Parent</em>' container reference.
+     * @see #setParent(Model1Parent)
+     * @see com.googlecode.hibernate.audit.test.model1.Model1Package#getModel1Child_Parent()
+     * @see com.googlecode.hibernate.audit.test.model1.Model1Parent#getChildren
+     * @model opposite="children" required="true" transient="false"
+     * @generated
+     */
+    Model1Parent getParent();
+
+    /**
+     * Sets the value of the '{@link com.googlecode.hibernate.audit.test.model1.Model1Child#getParent <em>Parent</em>}' container reference.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @param value the new value of the '<em>Parent</em>' container reference.
+     * @see #getParent()
+     * @generated
+     */
+    void setParent(Model1Parent value);
+
+} // Model1Child
