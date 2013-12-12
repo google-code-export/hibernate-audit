75bc56e36182309769e904c520c324d02e5d5664
kchobantonov@gmail.com <kchobantonov@gmail.com@5bd74da1-d548-0410-99f0-b708941b1824>
2013-12-12 17:14:54 +0000

    [maven-release-plugin]  copy for branch 1.0

---
+++ branches/1.0/src/main/java/com/googlecode/hibernate/audit/synchronization/work/RemoveCollectionAuditWorkUnit.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (C) 2009 Krasimir Chobantonov <kchobantonov@yahoo.com>
+ * This file is part of Hibernate Audit.
+
+ * Hibernate Audit is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Hibernate Audit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with Hibernate Audit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.googlecode.hibernate.audit.synchronization.work;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+import org.hibernate.Session;
+import org.hibernate.collection.PersistentCollection;
+import org.hibernate.engine.SessionImplementor;
+import org.hibernate.persister.collection.CollectionPersister;
+import org.hibernate.type.Type;
+
+import com.googlecode.hibernate.audit.HibernateAudit;
+import com.googlecode.hibernate.audit.configuration.AuditConfiguration;
+import com.googlecode.hibernate.audit.model.AuditEvent;
+import com.googlecode.hibernate.audit.model.AuditLogicalGroup;
+import com.googlecode.hibernate.audit.model.AuditTransaction;
+import com.googlecode.hibernate.audit.model.clazz.AuditType;
+import com.googlecode.hibernate.audit.model.object.EntityAuditObject;
+
+public class RemoveCollectionAuditWorkUnit extends AbstractCollectionAuditWorkUnit {
+    private String entityName;
+    private Serializable id;
+    private Object entity;
+    private PersistentCollection persistentCollection;
+    private AuditEvent auditEvent;
+    private CollectionPersister collectionPersister;
+
+    public RemoveCollectionAuditWorkUnit(String entityName, Serializable id, Object entity, PersistentCollection persistentCollection) {
+        this.entityName = entityName;
+        this.id = id;
+        this.entity = entity;
+        this.persistentCollection = persistentCollection;
+    }
+
+    @Override
+    protected String getEntityName() {
+        return entityName;
+    }
+
+    public void perform(Session session, AuditConfiguration auditConfiguration, AuditTransaction auditTransaction) {
+        initializeAuditEvents(session, auditConfiguration);
+        if (auditEvent != null) {
+            auditEvent.setAuditTransaction(auditTransaction);
+            auditTransaction.getEvents().add(auditEvent);
+            AuditLogicalGroup logicalGroup = getAuditLogicalGroup(session, auditConfiguration, auditEvent);
+
+            auditEvent.setAuditLogicalGroup(logicalGroup);
+        }
+    }
+
+    public void init(Session session, AuditConfiguration auditConfiguration) {
+        collectionPersister = ((SessionImplementor) session).getPersistenceContext().getCollectionEntry(persistentCollection).getLoadedPersister();
+    }
+
+    private void initializeAuditEvents(Session session, AuditConfiguration auditConfiguration) {
+        String role = collectionPersister.getCollectionMetadata().getRole();
+        String propertyName = role.substring(role.lastIndexOf('.') != -1 ? role.lastIndexOf('.') + 1 : 0, role.length());
+
+        if (!auditConfiguration.getExtensionManager().getAuditableInformationProvider().isAuditable(entityName, propertyName) || !persistentCollection.wasInitialized()) {
+            return;
+        }
+
+        AuditType auditType = HibernateAudit.getAuditType(session, auditConfiguration.getExtensionManager().getAuditableInformationProvider().getAuditTypeClassName(auditConfiguration.getAuditedConfiguration(), entityName));
+        auditEvent = new AuditEvent();
+        auditEvent.setAuditType(auditType);
+        auditEvent.setType(AuditEvent.REMOVE_AUDIT_EVENT_TYPE);
+        auditEvent.setEntityId(id == null ? null : id.toString());
+
+        EntityAuditObject auditObject = new EntityAuditObject();
+        auditObject.setAuditEvent(auditEvent);
+        auditObject.setAuditType(auditType);
+        auditObject.setTargetEntityId(id == null ? null : id.toString());
+        auditEvent.getAuditObjects().add(auditObject);
+
+        Type elementType = collectionPersister.getCollectionMetadata().getElementType();
+        Iterator<? extends Object> iterator = persistentCollection.entries(collectionPersister);
+
+        for (long i = 0; iterator.hasNext(); i++) {
+            Object element = iterator.next();
+
+            processElement(session, auditConfiguration, entity, element, elementType, propertyName, i, auditObject, auditEvent);
+        }
+    }
+}
---
+++ branches/1.0/src/test/java/com/googlecode/hibernate/audit/test/AuditLogicalGroupTest.java
@@ -0,0 +1,93 @@
+package com.googlecode.hibernate.audit.test;
+
+import org.eclipse.emf.teneo.mapping.strategy.EntityNameStrategy;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.testng.annotations.Test;
+
+import com.googlecode.hibernate.audit.HibernateAudit;
+import com.googlecode.hibernate.audit.configuration.AuditConfiguration;
+import com.googlecode.hibernate.audit.extension.auditable.AuditableInformationProvider;
+import com.googlecode.hibernate.audit.extension.event.AuditLogicalGroupProvider;
+import com.googlecode.hibernate.audit.listener.AuditListener;
+import com.googlecode.hibernate.audit.listener.AuditSessionFactoryObserver;
+import com.googlecode.hibernate.audit.model.AuditEvent;
+import com.googlecode.hibernate.audit.model.AuditLogicalGroup;
+import com.googlecode.hibernate.audit.model.clazz.AuditType;
+import com.googlecode.hibernate.audit.test.model1.Model1Factory;
+import com.googlecode.hibernate.audit.test.model1.Model1Package;
+import com.googlecode.hibernate.audit.test.model1.Model1Person;
+import com.googlecode.hibernate.audit.test.model1.impl.Model1PersonImpl;
+
+public class AuditLogicalGroupTest extends AbstractHibernateAuditTest {
+	private static final ThreadLocal<AuditLogicalGroup> AUDIT_LOGICAL_GROUP = new ThreadLocal<AuditLogicalGroup>();
+	
+	@Test
+	public void testSaveAuditLogicalGroup() {
+		AuditConfiguration config = AuditSessionFactoryObserver.getAuditConfiguration(dataStore.getSessionFactory());
+		config.getExtensionManager().setAuditLogicalGroupProvider(new TestAuditLogicalGroupProvider());
+		
+    	EntityNameStrategy entityNameStrategy = dataStore.getExtensionManager().getExtension(EntityNameStrategy.class);
+    	AuditableInformationProvider auditableInformationProvider = AuditListener.getAuditConfiguration(dataStore.getHibernateConfiguration()).getExtensionManager().getAuditableInformationProvider();
+		
+		AuditLogicalGroup auditLogicalGroup = new AuditLogicalGroup();
+		AuditType auditType = new AuditType();
+		auditType.setClassName(auditableInformationProvider.getAuditTypeClassName(dataStore.getHibernateConfiguration(), entityNameStrategy.toEntityName(Model1Package.eINSTANCE.getModel1Person())));
+		
+		auditLogicalGroup.setAuditType(auditType);
+		auditLogicalGroup.setExternalId("3252499999");
+		
+		try {
+			AUDIT_LOGICAL_GROUP.set(auditLogicalGroup);
+
+			Session s = dataStore.getSessionFactory().openSession();
+			Transaction t = s.beginTransaction();
+			HibernateAudit.getAuditType(s, auditableInformationProvider.getAuditTypeClassName(dataStore.getHibernateConfiguration(), entityNameStrategy.toEntityName(Model1Package.eINSTANCE.getModel1Person())));
+			t.commit();
+			s.close();
+
+			Model1Person person = createNewPerson();
+			
+			Session session = dataStore.getSessionFactory().openSession();
+			Transaction tx = session.beginTransaction();
+			
+			session.save(person);
+			tx.commit();
+			session.close();
+
+			
+		} finally {
+			AUDIT_LOGICAL_GROUP.set(null);
+		}
+	}
+	
+	private Model1Person createNewPerson() {
+		Session session = null;
+		try {
+			session = dataStore.getSessionFactory().openSession();
+			Transaction tx = session.beginTransaction();
+
+			Model1Person person = Model1Factory.eINSTANCE.createModel1Person();
+			person.setFirstName("First Name");
+			person.setLastName("Last Name");
+
+			session.save(person);
+
+			person.getId();
+			tx.commit();
+
+			return person;
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+	}
+
+	
+	private class TestAuditLogicalGroupProvider implements AuditLogicalGroupProvider {
+	    public AuditLogicalGroup getAuditLogicalGroup(Session session, AuditEvent auditEvent) {
+	    	return AUDIT_LOGICAL_GROUP.get();
+	    }
+	}
+}
